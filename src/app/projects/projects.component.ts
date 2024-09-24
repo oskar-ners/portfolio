ed54b27223a40ad990f638cb1bfb88b5d0459cb1
Oskar <ners.kontakt@wp.pl>
2024-09-24 12:08:16 +0200

    ecommerce-platform project added

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -20,14 +20,20 @@ export class ProjectsComponent {
     'assets/images/project1/project1-6.png',
     'assets/images/project1/project1-7.png',
   ];
-  projectTwoImages: string[] = ['assets/images/project1/project1-1.png'];
-  projectThreeImages: string[] = ['assets/images/project1/project1-2.png'];
-  projectFourImages: string[] = ['assets/images/project1/project1-3.png'];
-
   projectOneDescription: string =
     'This is a comprehensive to-do application that integrates Firebase Authentication and Firestore Database to manage user accounts and store tasks. Users can register, log in, and update their usernames securely through Firebase Authentication. The to-do list functionality allows users to add, edit, and delete tasks, with all data stored in real-time in Firestore Database. Additionally, the app features a WeatherService where users can input a city name to get real-time weather information, making it a versatile tool for both task management and daily planning.';
 
-  projectTwoDescription: string = 'Project two description';
+  projectTwoImages: string[] = [
+    'assets/images/project2/project2-0.png',
+    'assets/images/project2/project2-1.png',
+    'assets/images/project2/project2-2.png',
+    'assets/images/project2/project2-3.png',
+  ];
+  projectTwoDescription: string =
+    'This project is a fully functional e-commerce platform built with Firebase, where users can browse products, place orders and everything is stored in Firestore Database. It features Firebase Authentication for user registration and login, Firestore for real-time data storage of products, users, and orders, and allows seamless order processing and shopping experience.';
+
+  projectThreeImages: string[] = ['assets/images/project1/project1-2.png'];
   projectThreeDescription: string = 'Project two description';
+  projectFourImages: string[] = ['assets/images/project1/project1-3.png'];
   projectFourDescription: string = 'Project two description';
 }
